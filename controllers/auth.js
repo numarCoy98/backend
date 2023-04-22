20a4af47e5600bb2f10a7b9deb31034876dd4838
Stiven Coy <77942755+numarCoy98@users.noreply.github.com>
2023-04-21 22:59:39 -0500

    Validar contraseña al momento del login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const { response } = require('express') //para el intelices 
 const Usuario = require('../models/user')
+const bcrypt = require('bcryptjs')
+
 
 const crearUsuario = async (req, res = response) => {
     const { email, password, role } = req.body
@@ -13,6 +15,11 @@ const crearUsuario = async (req, res = response) => {
         }
         console.log('crear usuario', { usuario })
         usuario = new Usuario(req.body);
+        // Encriptar contraseña
+
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt)
+
         console.log({ usuario: usuario.id })
         await usuario.save()
         res.status(201).json({
@@ -31,9 +38,42 @@ const crearUsuario = async (req, res = response) => {
 
 }
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
     const { email, password } = req.body
-    res.status(201).json({ okay: true, msg: 'login', email, password })
+    try {
+        let usuario = await Usuario.findOne({ email })
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe (email)'
+            });
+        }
+
+        // confirmar los password
+        const validPassword = bcrypt.compareSync(password, usuario.password)
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Contraseña no valida'
+            })
+        }
+
+        // Generar nuestro JWT
+
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Error con el servidor'
+        })
+    }
 }
 
 const revalidarToken = (req, res) => {
